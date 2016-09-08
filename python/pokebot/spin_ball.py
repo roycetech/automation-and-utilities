800f76398f88d46bc427cff1002d265181f4ae09
Royce Remulla <royce.com@gmail.com>
2016-09-08 22:35:19 +0800

    Tested autopy3 to be faster than pyautogui

--- python/pokebot/spin_ball.py
+++ python/pokebot/spin_ball.py
@@ -1,31 +1,39 @@
 import pyautogui
 import time
 import math
+import autopy3
+
 
 # click bluestack app from the taskbar
-pyautogui.click(pyautogui.locateCenterOnScreen("images/blue_stacks_task_icon.png"))
+# pyautogui.click(pyautogui.locateCenterOnScreen("images/blue_stacks_task_icon.png"))
 
-pyautogui.mouseDown(1400, 560)
+# pyautogui.mouseDown(1400, 560)
 
 def getX(angle, radius):
-    return centerx + radius * math.cos(math.radians(angle))
+    return int(centerx + radius * math.cos(math.radians(angle)))
 
 def getY(angle, radius):
-    return centery + radius * math.sin(math.radians(angle))
+    return int(centery + radius * math.sin(math.radians(angle)))
 
-centerx=1400
-centery=490
+centerx=500
+centery=500
+
+autopy3.mouse.move(centerx, centery)
+
+print('Iikot!')
+# pyautogui.mouseDown(1400, 575)
+
+def spin(times):
+    for j in range(0, times+1):
+        for i in range(0, 360, 10):
+            x = getX(i, 50)
+            y = getY(i, 50)
+            autopy3.mouse.move(x, y)
+            time.sleep(0.005)
+
+spin(13)
 
 
-pyautogui.mouseDown(1400, 575)
-for i in range(0, 360, 90):
-    x = getX(i, 50)
-    y = getY(i, 50)
-    pyautogui.moveTo(x, y)
-for i in range(0, 360, 90):
-    x = getX(i, 50)
-    y = getY(i, 50)
-    pyautogui.moveTo(x, y)
 
 time.sleep(2)
 
